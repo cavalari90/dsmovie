a5ae32ff25b23047fd75387ef88c1316760dedd9
Guilherme Cavalari <guilhermecs90@gmail.com>
2022-01-15 22:18:41 -0300

    Props

--- frontend/src/components/MovieCard/index.jsx
+++ frontend/src/components/MovieCard/index.jsx
@@ -1,15 +1,26 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 
-function MovieCard() {
+//Props são argumentos do react
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+type Props = {
+    movie: Movie;
+}
+
+
+
+
+
+function MovieCard({ movie }: Props) {
+
+    //const movie = {
+    //    id: 1,
+    //    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    //    title: "The Witcher",
+    //    count: 2,
+    //    score: 4.5
+    //};
 
     return (
         <div>
@@ -18,7 +29,7 @@ function MovieCard() {
                 <h3>{movie.title}</h3>
                 <MovieScore />
                 <Link to={`/form/${movie.id}`} >
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
             </div>
         </div>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,29 +1,47 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
-//componente react é uma função javascript e então pode ter uma lógica por aqui...
-// .then() => é o que vai executar depois que a requisição voltar. (Assíncrono: o resto continua)
+    //componente react é uma função javascript e então pode ter uma lógica por aqui...
+    // .then() => é o que vai executar depois que a requisição voltar. (Assíncrono: o resto continua)
 
-//useState passa valores iniciais
-const [pageNumber, setPageNumber] = useState(0);
+    //useState passa valores iniciais
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
 
 
-axios.get(`${BASE_URL}/movies?size=5&page=1`)
-.then(response => {
-    const data = response.data as MoviePage;
-    setPageNumber(data.number);
-   // console.log(response.data);
-});
+
+    //recebe 2 coisas: função a ser executada e lista de objetos a observar. Sempre que altera algo na lista, reexecuta func
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)  //garantindo que a busca vai ser ordenada pelo id. sempre vir na mesma ordem
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+
+            });
+    }, [pageNumber]);
+    // tá falando que quando mudar pageNumber, precisa refazer a requisição
+
 
 
     return (
         <>
-            <p>{pageNumber}</p>
+
             <Pagination />
 
 
@@ -31,30 +49,15 @@ axios.get(`${BASE_URL}/movies?size=5&page=1`)
                 <div className="row">
                     {/* Grids e Breakpoints em React*/}
                     {/* código abaixo diz que a partir do tamanho sm (small), cada card vai ocupar 6 dos 12 grids...*/}
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    ))}
+
+
+
+
 
                 </div>
 
